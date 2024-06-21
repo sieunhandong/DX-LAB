1ab4083a4b83fed76ecf4ce137ac9889df2292ef
anhnguyen52 <154457042+anhnguyen52@users.noreply.github.com>
2024-06-21 13:27:53 +0700

    update -logincontrol

--- src/java/controllerCommon/LoginControl.java
+++ src/java/controllerCommon/LoginControl.java
@@ -4,6 +4,7 @@
  */
 package controllerCommon;
 
+import dal.CandidateDAO;
 import dal.DAO;
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -13,6 +14,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 import models.Account;
+import models.Applications;
 
 /**
  *
@@ -43,8 +45,13 @@ public class LoginControl extends HttpServlet {
         } else {
             HttpSession session = request.getSession();
             String role_name = dao.getRoleName(acc.getRole_id());
+            //lấy projectcode
+            CandidateDAO cdao = new CandidateDAO();
+            Applications projectCode = cdao.getProjectCodeByApplicantId(acc.getUser_id());
+            session.setAttribute("projectCode", projectCode);
             session.setAttribute("account", acc);
             session.setAttribute("role_name", role_name);
+            
              if (isFirstLogin(acc)) {
                 response.sendRedirect("updateProfileFirstTime.jsp");
             } else {
