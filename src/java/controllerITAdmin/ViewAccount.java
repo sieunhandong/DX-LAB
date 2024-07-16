e81fbb7dba35c8a2d047e3fe440b9a13d86a4131
Ton Khanh Linh <linhtkhe176489@fpt.edu.vn>
2024-07-16 15:58:14 +0700

    iter3

---
+++ src/java/controllerITAdmin/ViewAccount.java
@@ -0,0 +1,45 @@
+package controllerITAdmin;
+
+import dal.AdminDAO;
+import jakarta.servlet.RequestDispatcher;
+import java.io.IOException;
+import java.util.List;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import models.Account;
+
+@WebServlet("/viewAccount")
+public class ViewAccount extends HttpServlet {
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+
+        AdminDAO adminDao = new AdminDAO();
+        List<Account> listAccount = adminDao.getAllAccount();
+
+        request.setAttribute("list", listAccount);
+        RequestDispatcher dispatcher = request.getRequestDispatcher("/viewAccount.jsp");
+        dispatcher.forward(request, response);
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    @Override
+    public String getServletInfo() {
+        return "Servlet for viewing user accounts";
+    }
+}
